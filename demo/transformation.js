dbfd5e467f211f254c4cd9a75d8f0ba53c836a6d
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-04-04 10:01:45 +1000

    move the transaformation to spereate file

---
+++ demo/transformation.js
@@ -0,0 +1,48 @@
+const oneStep={
+    step: 0,
+    duration: 1,
+    A:0,
+    B:0,
+    C:0,
+    D:0,
+    Asharp:0,
+    Bsharp:0,
+    Csharp:0,
+    Dsharp:0
+};
+
+function createOctave(tone,step,sharp){
+    return `${tone}${step}${sharp==1?"#":''}`;
+}
+
+export const string2Octave=function(data){
+    const mData = data.split("").map(item=>{
+        let tone;
+        switch(item){
+            case "T" : tone = "A";break;
+            case "C" : tone = "B";break;
+            case "G" : tone = "C";break;
+            case "A" : tone = "D";break;
+            default: tone = "A";
+        }
+        if(oneStep[tone]>=4) {
+            if(oneStep[`${tone}sharp`]==1){
+                oneStep[tone]=1;
+                oneStep[`${tone}sharp`]=0;
+            }else{
+                oneStep[tone]=1;
+                oneStep[`${tone}sharp`]=1;
+            }
+        }else{
+            oneStep[tone]++;
+        }
+        oneStep.step++;
+        if(oneStep.step>30) oneStep.step=1;
+        return [
+            oneStep.step,
+            2,
+            [createOctave(tone.toLowerCase(),oneStep[tone],oneStep[`${tone}sharp`])]
+        ];
+    });
+};
+
